ec5a0e96508c0e83e5625193b7f6d3b4fc2d58d2
adisu-mulu <kulhabesh31@gmail.com>
2024-03-28 16:27:38 +0300

    updting pint

--- pint.c
+++ pint.c
@@ -4,5 +4,5 @@ void pint(stack_t **stack, unsigned int line_number)
 	stack_t *temp = *stack;
 	if(temp != NULL)
 		printf("%d\n", temp->n);
-	else printf("L<%d>: can't pint, stack empty\n", line_number);
+	else fprintf(stderr, "L<%d>: can't pint, stack empty\n", line_number);
 }
