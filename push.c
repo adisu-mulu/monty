f05ea63f6be9c4b734ef72892b4617e3668f4a9d
adisu-mulu <kulhabesh31@gmail.com>
2024-03-29 09:39:52 +0300

    updating push

--- push.c
+++ push.c
@@ -1,13 +1,19 @@
 #include "monty.h"
+/**
+ * push_function - function
+ * @stack: pointer
+ * @line_number: integer
+ * Return: void
+ */
 void push_function(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
+
 	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-
 	new_node->n = data;
 
 	new_node->prev = NULL;
@@ -19,5 +25,4 @@ void push_function(stack_t **stack, unsigned int line_number)
 
 	if (line_number > 0)
 	{}
-	
 }
