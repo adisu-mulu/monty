b2cd7274baeca509bd16a1922514db41f202532d
adisu-mulu <kulhabesh31@gmail.com>
2024-03-31 22:06:43 +0300

    updating pchar

--- pchar.c
+++ pchar.c
@@ -22,7 +22,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		fprintf(stderr, " L%d: can't pchar, stack empty\n", line_number);
 		freeStack(stack, line_number);
 		exit(EXIT_FAILURE);
 	}
