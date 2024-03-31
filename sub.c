9f41f676ee07cf4ea2d1b37990bf7c54b244e621
adisu-mulu <kulhabesh31@gmail.com>
2024-03-31 13:36:00 +0300

    adding sub

---
+++ sub.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+/**
+ * sub - function
+ * @stack: pointer
+ * @line_number: integer
+ * Return: void
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+	stack_t *temp2 = *stack;
+	int count = 0;
+	int value;
+
+	if (temp == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		freeStack(stack, line_number);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		while (temp != NULL)
+		{
+			temp = temp->next;
+			count++;
+		}
+		if (count < 2)
+		{
+			fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+			freeStack(stack, line_number);
+			exit(EXIT_FAILURE);
+
+		}
+		else
+		{
+			value = temp2->next->n;
+			temp2->next->n = value - temp2->n;
+			*stack = temp2->next;
+			free(temp2);
+		}
+	}
+}
