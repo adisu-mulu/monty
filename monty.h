12df42b1fd2915d8616e29d1df1d9b9aab951887
adisu-mulu <kulhabesh31@gmail.com>
2024-04-03 09:46:46 +0300

    changes to main

--- monty.h
+++ monty.h
@@ -41,6 +41,7 @@ void sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
 void _mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 int isOpcodeOnly(const char *str);
 void pop(stack_t **stack, unsigned int line_number);
