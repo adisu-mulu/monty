53277de7aa86125b05d7f1dd0f6873c44dd6134d
adisu-mulu <kulhabesh31@gmail.com>
2024-04-03 11:16:55 +0300

    adding pstr

---
+++ pstr.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+ * pstr - function
+ * @stack: pointer
+ * @line_number: integer
+ * Return: void
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (temp == NULL)
+		printf("\n");
+	else
+	{
+
+		while (temp != NULL)
+		{
+			if (temp->n > 0 && temp->n <= 127)
+			{
+				printf("%c", (char)temp->n);
+				temp = temp->next;
+			}
+			else
+			{
+				printf("\n");
+				freeStack(stack, line_number);
+				exit(1);
+			}
+		}
+		printf("\n");
+	}
+}
